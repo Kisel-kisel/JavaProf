9c0e0a9202eac7146789a1920d89b40fd0cd05a7
Kisel <kvpkvp6@gmail.com>
2023-03-11 16:16:00 +0100

    homeWork15

--- src/homeWorks/homeWork15/Task1/FuncInt.java
+++
@@ -1,5 +0,0 @@
-package homeWorks.homeWork15.Task1;
-
-public interface FuncInt {
-    void some(String[] strings);
-}
--- src/homeWorks/homeWork15/Task2/Dictionary.java
+++
@@ -1,4 +0,0 @@
-package homeWorks.homeWork15.Task2;
-
-public class Dictionary {
-}
