e5cda9d47c4fe26bfb4916f105e7929302e0657e
Kisel <kvpkvp6@gmail.com>
2023-05-04 20:53:57 +0200

    new

--- src/lessons/reflection/homeWork/ForAnnotaition.java
+++ src/lessons/reflection/homeWork/ForAnnotaition.java
@@ -1,5 +1,7 @@
 package lessons.reflection.homeWork;
 
-@HomeWork("новая аннотация")
+
 public class ForAnnotaition {
+    @HomeWork("новая аннотация")
+    String str = "qqq";
 }
--- src/lessons/reflection/homeWork/HomeWork.java
+++ src/lessons/reflection/homeWork/HomeWork.java
@@ -1,10 +1,12 @@
 package lessons.reflection.homeWork;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.FIELD;
-
+@Retention(RetentionPolicy.RUNTIME)
 @Target(value = {ANNOTATION_TYPE,FIELD})
 public @interface HomeWork {
     String value();
