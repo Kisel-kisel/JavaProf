b7ebbf780dc15ce4e418a44530394c3e5164f016
Kisel <kvpkvp6@gmail.com>
2023-05-04 20:51:25 +0200

    new

--- src/lessons/reflection/homeWork/HomeWork.java
+++ src/lessons/reflection/homeWork/HomeWork.java
@@ -1,5 +1,11 @@
 package lessons.reflection.homeWork;
 
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+
+@Target(value = {ANNOTATION_TYPE,FIELD})
 public @interface HomeWork {
     String value();
 }
